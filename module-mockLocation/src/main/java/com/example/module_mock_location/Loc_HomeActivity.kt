2cb50f8cd3c25806bb82d6d8ee98755fb54e0852
lilei1990 <416587959@qq.com>
2019-05-05 00:10:49 +0800

    1

--- module-mockLocation/src/main/java/com/example/module_mock_location/Loc_HomeActivity.kt
+++ module-mockLocation/src/main/java/com/example/module_mock_location/Loc_HomeActivity.kt
@@ -1,7 +1,6 @@
 package com.example.module_mock_location
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.config.ARouterConfig
@@ -13,12 +12,8 @@ class Loc_HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loc__home)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
 }
